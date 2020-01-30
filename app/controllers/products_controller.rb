f16336df2e7e12d3a7c75737df00367b89250c61
manish4uanand <manish4uanand@gmail.com>
2020-01-31 02:42:45 +0530

    add product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,12 @@ class ProductsController < ApplicationController
     render json: @products, status: 200
   end
 
+  def show
+    @product = Product.find(params[:id])
+    render json: @product, status: 200
+  end
+
+
 end
 
 
